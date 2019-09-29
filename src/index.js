9dea21833beccda6310d385e3eda38ad16063be0
Sam Noh <samnoh21@outlook.com>
2019-09-29 15:12:50 +1300

    Update chunkHelper and rename files

--- src/index.js
+++ src/index.js
@@ -2,17 +2,17 @@ const Page = require('./page');
 const Output = require('./output');
 const { UOA_COURSE_CATALOGUE, QUERY, ALPHABETS, FILEPATH } = require('./constants');
 
+const chunkHelper = text =>
+    text
+        .filter(t => t.length > 1 && t !== 'Skip to Main Content')
+        .map(t => `'${t.split('-')[0].trim()}',`)
+        .join('');
+
 (async () => {
     const output = new Output(FILEPATH, 'export default [', ']');
 
     const page = await Page.build();
     await page.goto(UOA_COURSE_CATALOGUE);
-
-    const chunkHelper = text =>
-        text
-            .filter(t => t.length > 1 && t !== 'Skip to Main Content')
-            .map(t => `'${t.split('-')[0].trim()}',`)
-            .join('');
 
     for (char of ALPHABETS) {
         console.log(((ALPHABETS.indexOf(char) / 26) * 100).toFixed(2) + '%...');
