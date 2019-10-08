a789fb594f7c857ee6b405dc204439892105256d
Sam Noh <samnoh21@outlook.com>
2019-10-08 14:17:39 +1300

    refactor(chunkhelper.js): change data structure to key-value for perf

--- src/utils/chunkHelper.js
+++ src/utils/chunkHelper.js
@@ -1,7 +1,7 @@
 export default (text) => text
     .filter((t) => t.length > 1 && t !== 'Skip to Main Content')
     .map(
-        (t) => `{subject: '${t.split('-')[0].trim()}', title: '${t
+        (t) => `{'${t.split('-')[0].trim()}': '${t
             .split('-')[1]
             .trim()
             .replace('&amp;', '&')}'},`,
