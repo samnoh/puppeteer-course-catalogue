1523548695eb19ff4fc7cd3376c1840aedf97e99
Sam Noh <samnoh21@outlook.com>
2020-07-21 15:07:05 +1200

    update output format

--- src/utils/chunkHelper.js
+++ src/utils/chunkHelper.js
@@ -1,9 +1,11 @@
-export default (text) => text
-    .filter((t) => t.length > 1 && t !== 'Skip to Main Content')
-    .map(
-        (t) => `'${t.split('-')[0].trim()}': '${t
-            .split('-')[1]
-            .trim()
-            .replace('&amp;', '&')}',`,
-    )
-    .join('');
+export default (text) =>
+    text
+        .filter((t) => t.length > 1 && t !== 'Skip to Main Content')
+        .map(
+            (t) =>
+                `{subject: '${t.split('-')[0].trim()}', title: '${t
+                    .split('-')[1]
+                    .trim()
+                    .replace('&amp;', '&')}' },`
+        )
+        .join('');
